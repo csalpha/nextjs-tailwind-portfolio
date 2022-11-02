576038a94264348ebbbfa39593d63bacfa4467a5
Carlos Serôdio <carlosserodio86@hotmail.com>
2022-11-02 17:47:33 +0000

    Changes

--- components/Main.jsx
+++ components/Main.jsx
@@ -14,7 +14,7 @@ const Main = () => {
             LET´S WORK TOGETHER
           </p>
           <h1 className='py-4 text-gray-700'>
-            Hello, I'm <span className='text-[#5651e5]'>Carlos</span>
+            Hello, I am <span className='text-[#5651e5]'>Carlos</span>
           </h1>
           <h1 className='py-2 text-gray-700'>A Full Stack Developer</h1>
           <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
